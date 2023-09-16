30c8437f5a94e71076d0ad82d46d96220ec66f8d
HengYu <hengyu@ailinking.com>
2023-09-16 14:18:12 +0800

    feat:  add dashboard
    这是我的提交描述信息

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     routes: [
         {
             path: '/',
-            redirect: '/demo'
+            redirect: '/HomeVIew'
         },
         {
             path: '/table',
@@ -23,7 +23,15 @@ const router = createRouter({
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
             component: () => import('../views/Demo.vue')
-        }
+        },
+        {
+            path: '/homeView',
+            name: 'homeView',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/HomeView.vue')
+        },
     ]
 })
 
--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -4,7 +4,13 @@
 
 <template>
   <div>
-    <h3>hello wolrd</h3>
+    <h3>hello world</h3>
+  </div>
+  <div>
+    <p class="text-blue-600">The quick brown fox...</p>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </div>
 </template>
 
